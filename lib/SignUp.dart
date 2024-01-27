d9e49b60ea4355b2a9a1bc9ff40b548bfcd955c9
kushm121 <makkarkush@gmail.com>
2024-01-27 20:00:38 +0530

    modified:   lib/SignUp.dart

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'landing.dart';
+import 'Login.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -9,8 +10,250 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  late TextEditingController _usernameController;
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  late TextEditingController _confirmPasswordController;
+
+  bool _isUsernameTyped = false;
+  bool _isEmailTyped = false;
+  bool _isPasswordTyped = false;
+  bool _isConfirmPasswordTyped = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController = TextEditingController();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 15, 11, 33),
+          ),
+          child: ListView(
+            physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics(),
+            ),
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  bottom: 20,
+                  left: 10,
+                  right: 10,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Colors.white, // Outline color
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _usernameController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: _isUsernameTyped ? '' : 'Username',
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _isUsernameTyped = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                    ),
+
+                    // SizedBox for spacing
+                    SizedBox(height: 20),
+
+                    // Add the custom outlined transparent text field for Email
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Colors.white, // Outline color
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _emailController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: _isEmailTyped ? '' : 'Email',
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _isEmailTyped = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                    ),
+
+                    // SizedBox for spacing
+                    SizedBox(height: 20),
+
+                    // Add the custom outlined transparent text field for Password
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Colors.white, // Outline color
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _passwordController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: _isPasswordTyped ? '' : 'Password',
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _isPasswordTyped = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                    ),
+
+                    // SizedBox for spacing
+                    SizedBox(height: 20),
+
+                    // Add the custom outlined transparent text field for Confirm Password
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Colors.white, // Outline color
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _confirmPasswordController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText:
+                              _isConfirmPasswordTyped ? '' : 'Confirm Password',
+                          hintStyle: TextStyle(color: Colors.white),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _isConfirmPasswordTyped = value.isNotEmpty;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 114, 105, 255),
+                        borderRadius: BorderRadius.circular(100),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.25),
+                            blurRadius: 4,
+                            offset: Offset(0, 4), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUp()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          elevation: 0,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Circular',
+                              fontWeight: FontWeight.w800,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Circular',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Login()),
+                            );
+                          },
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 114, 105, 255),
+                              fontFamily: 'Circular',
+                              fontWeight: FontWeight.w900,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
